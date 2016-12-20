5db55dd84411ce256d2d4e8ebf7899e691697290
ranty <ranty720@gmail.com>
2016-12-21 00:26:57 +0800

    some

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
          post :quit
        end
   
-    resources :posts
-  end
-    namespace :account do
-      resources :groups
-  end
+       resources :posts
+     end
+     namespace :account do
+       resources :groups
+       resources :posts
+    end
 
 
   root 'groups#index'
